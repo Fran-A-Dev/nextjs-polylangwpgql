d6c3828b409d973c61acbe270ed439d4f594c01e
Fran Agulto <fran.agulto@fran-agulto.attlocal.net>
2022-06-09 10:22:33 -0500

    added language

---
+++ components/Navbar.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function Navbar() {
+  const { locale: activeLocale, locales, asPath } = useRouter();
+
+  const availableLocales = locales.filter((locale) => locale !== activeLocale);
+
+  return (
+    <div className="container">
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contact Us</a>
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          {availableLocales.map((locale) => {
+            return (
+              <li key={locale}>
+                <Link href={asPath} locale={locale}>
+                  <a>{locale.toUpperCase()}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
+}
