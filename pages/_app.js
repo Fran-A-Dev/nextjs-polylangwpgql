566d0954528c7e4a863eeb31e9c1b63284c96bbc
Fran Agulto <fran.agulto@fran-agulto.attlocal.net>
2022-06-10 06:47:52 -0500

    changed navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import "../styles/globals.css";
-import Navbar from "../components/Navbar";
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
+   
       <Component {...pageProps} />
     </>
   );
