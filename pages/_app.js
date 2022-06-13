ae7bbab36177430b4eece25bb56ef34e53a443d9
Fran Agulto <fran.agulto@fran-agulto.attlocal.net>
2022-06-13 15:32:23 -0500

    added dynamic product page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "../styles/globals.css";
-
+import Navbar from "../components/Navbar"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-   
+  <Navbar />
       <Component {...pageProps} />
+      
     </>
   );
 }
